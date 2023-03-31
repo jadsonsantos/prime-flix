02110e7b3e2a4daafc277b99f15cd16047bf3f01
Jadson Santos <jadsondossantos5@gmail.com>
2023-03-30 22:38:12 -0300

    listing movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,46 @@
-const Home = () => (
-  <div>
-    <h1>pagina home</h1>
-  </div>
-)
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import api from '../../services/api'
+import './Home.scss'
+
+const Home = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get('/movie/now_playing', {
+        params: {
+          api_key: '9c366f3c92674ee5238386c2ef0ce2f1',
+          language: 'pt-BR',
+          page: 1,
+        },
+      })
+
+      setMovies(response.data.results.slice(0, 10))
+    }
+
+    loadMovies()
+  }, [])
+
+  return (
+    <div>
+      <div className="movies">
+        {movies.map(({ title, id, poster_path }) => (
+          <article key={id} className="movies__item movie">
+            <strong className="movie__title">{title}</strong>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+              alt={title}
+              className="movie__image"
+            />
+            <Link to={`/movie/${id}`} className="movie__link">
+              Acessar
+            </Link>
+          </article>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default Home
