5eb5ae9600086be0adae2664cf5d9ebd406899c1
Jadson Santos <jadsondossantos5@gmail.com>
2023-03-30 21:49:35 -0300

    create pages

---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div>
+    <h1>pagina home</h1>
+  </div>
+)
+
+export default Home
---
+++ src/pages/Movie/index.js
@@ -0,0 +1,8 @@
+const Movie = () => (
+  <div>
+    <p>teste</p>
+    <h1>pagina do filme</h1>
+  </div>
+)
+
+export default Movie
