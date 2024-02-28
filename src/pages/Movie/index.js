be4ab6158e4556d264f43bb98584e6a67ef9c76c
Jadson Santos <jadsondossantos5@gmail.com>
2024-02-28 19:18:14 -0300

    feat: adjustments

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -2,12 +2,13 @@ import { formateDate } from 'utils'
 import './Movie.scss'
 import useMovie from './useMovie'
 import formatCurrency from 'utils/formatCurrency'
+import Loading from 'components/Loading'
 
 const Movie = () => {
   const { saveMovie, loading, movie } = useMovie()
 
   if (loading) {
-    return <section className="loading">Carregando detalhes...</section>
+    return <Loading text="Carregando detalhes..." />
   }
 
   const {
@@ -34,12 +35,23 @@ const Movie = () => {
         width={800}
         height={450}
       />
-      <span>Título original: {original_title}</span> |{' '}
-      <span>{formateDate(release_date)}</span> | <span>{runtime} minutos</span>{' '}
       <h3>Sinopse</h3>
       <p>{overview}</p>
       {tagline && <p>Slogan: {tagline}</p>}
       <h3>Detalhes</h3>
+      <p>Título original: {original_title}</p>
+      <p>Data de lançamento: {formateDate(release_date)}</p>
+      <p>Duração: {runtime} minutos</p>{' '}
+      <p>
+        Países de origem:{' '}
+        {production_countries.map((item) => (
+          <span key={item.name}>{item.name} | </span>
+        ))}
+      </p>
+      {budget > 0 && <p>Budget: {formatCurrency(budget)}</p>}
+      <p className="movie__average">
+        Avaliação: {vote_average.toFixed(1)} / 10
+      </p>
       <p>
         Gêneros:
         {genres.map((genre) => (
@@ -48,16 +60,6 @@ const Movie = () => {
           </span>
         ))}
       </p>
-      {budget > 0 && <p>Budget: {formatCurrency(budget)}</p>}
-      <p>
-        Países de origem:{' '}
-        {production_countries.map((item) => (
-          <span key={item.name}>{item.name} | </span>
-        ))}
-      </p>
-      <strong className="movie__average">
-        Avaliação: {vote_average.toFixed(1)} / 10
-      </strong>
       <div className="movie__buttons">
         <button onClick={saveMovie}>Salvar</button>
         <button>
