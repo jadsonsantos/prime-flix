46c606ca759be47fc586e70037580c3da0701a13
Jadson Santos <jadsondossantos5@gmail.com>
2024-03-02 00:11:16 -0300

    feat: add loading plugin

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,8 +1,14 @@
+import { ClipLoader } from 'react-spinners'
 import PropTypes from 'prop-types'
 import './Loading.scss'
 
 const Loading = ({ text }) => {
-  return <section className="loading">{text}</section>
+  return (
+    <section className="loading">
+      {text}
+      <ClipLoader size={50} color="#000" />
+    </section>
+  )
 }
 
 Loading.propTypes = {
