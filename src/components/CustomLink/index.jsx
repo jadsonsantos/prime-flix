74b60b6bd45ea7d58b5be376465e85075a314166
Jadson Santos <jadsondossantos5@gmail.com>
2024-03-30 18:51:33 -0300

    feat: components adjustments

--- src/components/CustomLink/index.jsx
+++ src/components/CustomLink/index.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types'
 
-const CustomLink = ({ children, href, target = '_blank', className }) => {
+const CustomLink = ({
+  children,
+  href,
+  target = '_blank',
+  className,
+  ...rest
+}) => {
   return (
     <a
       href={href}
       target={target}
       className={className}
       rel={target === '_blank' ? 'noopener noreferrer' : ''}
+      {...rest}
     >
       {children}
     </a>
@@ -14,7 +21,7 @@ const CustomLink = ({ children, href, target = '_blank', className }) => {
 }
 
 CustomLink.propTypes = {
-  children: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
   href: PropTypes.string.isRequired,
   target: PropTypes.string,
   className: PropTypes.string,
