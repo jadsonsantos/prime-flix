a7002338bc11972eb9461b348de21aed721e15ea
Jadson Santos <jadsondossantos5@gmail.com>
2024-04-01 12:11:37 -0300

    feat: add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import MenuContextProvider from 'contexts/menu'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <MenuContextProvider>
