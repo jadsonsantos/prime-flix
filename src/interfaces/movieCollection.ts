f262d38da5bc0d97335a96083831e70b9bed3c39
Jadson Santos <jadsondossantos5@gmail.com>
2024-04-04 09:26:06 -0300

    feat: add movie collection interface

---
+++ src/interfaces/movieCollection.ts
@@ -0,0 +1,12 @@
+interface MovieCollection {
+  adult: boolean
+  backdrop_path: string
+  id: number
+  name: string
+  original_language: string
+  original_name: string
+  overview: string
+  poster_path: string
+}
+
+export default MovieCollection
