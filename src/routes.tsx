70561dc66b841b93f74f801cae8d66e1045fd1e2
Jadson Santos <jadsondossantos5@gmail.com>
2026-03-04 20:16:54 -0300

    feat: new routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import MoviesContainer from 'pages/MoviesContainer'
 import NotFound from 'pages/NotFound'
 import Person from 'pages/Person'
 import Search from 'pages/Search'
+import SeriesContainer from 'pages/SeriesContainer'
 
 import Footer from 'components/Footer'
 import Header from 'components/Header'
@@ -20,12 +21,27 @@ import {
   TOP_RATED_ROUTE,
   UPCOMING_ROUTE,
 } from 'constants/routes'
+import {
+  AIRING_TODAY,
+  ON_THE_AIR,
+  POPULAR_TV,
+  TOP_RATED_TV,
+} from 'constants/tvFilters'
+import {
+  AIRING_TODAY_ROUTE,
+  ON_THE_AIR_ROUTE,
+  POPULAR_TV_ROUTE,
+  TOP_RATED_TV_ROUTE,
+  TV_DETAILS_ROUTE,
+  TV_HOME_ROUTE,
+} from 'constants/tvRoutes'
 
 const RoutesApp = () => (
   <BrowserRouter>
     <Header />
     <main className="main">
       <Routes>
+        {/* Rotas existentes de filmes */}
         <Route path={HOME_ROUTE} element={<Home />} />
         <Route
           path={NOW_PLAYING_ROUTE}
@@ -44,6 +60,40 @@ const RoutesApp = () => (
           element={<MoviesContainer title="Em breve" filter={UPCOMING} />}
         />
         <Route path="/movie/:id" element={<Movie />} />
+        <Route path={TV_DETAILS_ROUTE} element={<Movie />} />
+
+        {/* Novas rotas de séries */}
+        <Route
+          path={TV_HOME_ROUTE}
+          element={<SeriesContainer title="Séries" filter={POPULAR_TV} />}
+        />
+        <Route
+          path={AIRING_TODAY_ROUTE}
+          element={
+            <SeriesContainer title="Exibindo Hoje" filter={AIRING_TODAY} />
+          }
+        />
+        <Route
+          path={ON_THE_AIR_ROUTE}
+          element={<SeriesContainer title="No Ar" filter={ON_THE_AIR} />}
+        />
+        <Route
+          path={POPULAR_TV_ROUTE}
+          element={
+            <SeriesContainer title="Séries Populares" filter={POPULAR_TV} />
+          }
+        />
+        <Route
+          path={TOP_RATED_TV_ROUTE}
+          element={
+            <SeriesContainer
+              title="Séries Mais Votadas"
+              filter={TOP_RATED_TV}
+            />
+          }
+        />
+
+        {/* Outras rotas existentes */}
         <Route path={FAVORITES_ROUTE} element={<Favorites />} />
         <Route path="/search" element={<Search />} />
         <Route path="/person/:id" element={<Person />} />
